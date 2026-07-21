91c10a906b9a5bc91dd9083eaa2f06462bae35f9
Mariia Lazareva <mariika383@gmail.com>
2026-07-21 13:03:11 +0300

    1st task - Strings

---
+++ strings/subtask_1.py
@@ -0,0 +1,6 @@
+def string_length(text):
+    return len(text)
+
+print(string_length("Supercalifragilisticexpialidocious"))
+
+
---
+++ strings/subtask_2.py
@@ -0,0 +1,4 @@
+def join_strings(str1, str2):
+    return str1 + str2
+
+print(join_strings("What`s ", "up? "))
